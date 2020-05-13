efb56a14bad2598fb3c4ef8c36bdb62ab9d8ce3c
Quentin CLEMENT <quentinclement.pro@gmail.com>
2020-05-13 17:13:19 +0200

    Add init for WrappedValue

--- Sources/ResilientDecoding/Resilient.swift
+++ Sources/ResilientDecoding/Resilient.swift
@@ -31,6 +31,11 @@ public struct Resilient<Value: Decodable>: Decodable {
     self.outcome = outcome
   }
 
+  public init(wrappedValue: Value) {
+        self.wrappedValue = wrappedValue
+        self.outcome = .decodedSuccessfully
+  }
+
   public var wrappedValue: Value
 
   let outcome: ResilientDecodingOutcome
